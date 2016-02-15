dc06a7589c95046def89cfe1d45bab09ccf9a627
chajr <chajr@bluetree.pl>
2016-02-15 19:27:33 +0100

    Added query factory builder class

---
+++ src/Database/Query.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database;
+
+use Aura\SqlQuery\QueryFactory;
+
+class Query
+{
+    /**
+     * @var Connect
+     */
+    protected $_connection;
+
+    /**
+     * @var QueryFactory
+     */
+    protected $_factory;
+
+    /**
+     * Create sql query factory
+     */
+    public function __construct()
+    {
+        $this->_connection = new Connect;
+        $this->_factory = new QueryFactory('mysql');
+    }
+
+    /**
+     * return select object
+     *
+     * @return \Aura\SqlQuery\Common\SelectInterface
+     */
+    public function select()
+    {
+        return $this->_factory->newSelect();
+    }
+
+    /**
+     * return insert object
+     *
+     * @return \Aura\SqlQuery\Common\InsertInterface
+     */
+    public function insert()
+    {
+        return  $this->_factory->newInsert();
+    }
+}

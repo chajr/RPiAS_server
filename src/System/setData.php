24113dae37513faa41402aa23d3fb5d8be95856a
chajr <chajr@bluetree.pl>
2016-02-15 19:55:09 +0100

    Created basic insert system information class

--- src/System/setData.php
+++ src/System/setData.php
@@ -2,15 +2,40 @@
 
 namespace System;
 
+use Database\Connect;
+use Config\Config;
+
 class setData
 {
+    /**
+     * setData constructor.
+     *
+     * @param \Aura\Web\Request $request
+     * @param \Aura\Web\Response $response
+     * @param \Aura\View\View $view
+     */
     public function __construct($request, $response, $view)
     {
+        $status = 'success';
+        $message = 'Data written successfully.';
+
+        $secureToken = (new Config)->getConfig()['secure_token'];
+        $retrievedSecureToken = $request->query->get('secure', '');
+        
+        if ($secureToken !== $retrievedSecureToken) {
+            $status = 'error';
+            $message = 'Incorrect secure token';
+        } else {
+            echo ($request->post->get('key2', 'default'));
+            //        $q = (new\Database\Query)->select()->cols(['*'])->from('system_log');
+            //        var_dump((new Connect)->query($q));
+        }
+
         $view->setData([
-            'status' => 'success',
-            'message' => 'message from system',
+            'status' => $status,
+            'message' => $message,
         ]);
-        $request->query->get('key', 'default');
+
         $response->content->set($view->__invoke());
     }
 }

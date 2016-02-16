70d40618c2cdaef3ddc7ccb76178e65fb1bd17ed
chajr <chajr@bluetree.pl>
2016-02-16 18:23:40 +0100

    Added query to save system information

--- src/System/setData.php
+++ src/System/setData.php
@@ -21,14 +21,36 @@ class setData
 
         $secureToken = (new Config)->getConfig()['secure_token'];
         $retrievedSecureToken = $request->query->get('secure', '');
-        
+
         if ($secureToken !== $retrievedSecureToken) {
             $status = 'error';
             $message = 'Incorrect secure token';
         } else {
-            echo ($request->post->get('key2', 'default'));
-            //        $q = (new\Database\Query)->select()->cols(['*'])->from('system_log');
-            //        var_dump((new Connect)->query($q));
+            $post = $request->post;
+
+            $query = (new\Database\Query)
+                ->insert()
+                ->into('system_log')
+                ->cols([
+                    'date' => $post->get('date', 'NOW()'),
+                    'cpu_utilization' => $post->get('cpu_utilization', 0),
+                    'memory_free' => $post->get('memory_free', 0),
+                    'memory_used' => $post->get('memory_used', 0),
+                    'uptime_p' => $post->get('uptime_p', ''),
+                    'uptime_s' => $post->get('uptime_s', ''),
+                    'system_load' => $post->get('system_load', ''),
+                    'process_number' => $post->get('process_number', 0),
+                    'disk_utilization' => $post->get('disk_utilization', ''),
+                    'network_utilization' => $post->get('network_utilization', ''),
+                    'logged_in_users' => $post->get('logged_in_users', ''),
+                    'logged_in_users_count' => $post->get('logged_in_users_count', 0),
+                    'users_work' => $post->get('users_work', ''),
+                    'hostname' => $post->get('hostname', ''),
+                    'ip_internal' => $post->get('ip_internal', ''),
+                    'ip_external' => $post->get('ip_external', ''),
+                    'extra' => $post->get('extra', ''),
+                ]);
+            (new Connect)->query($query);
         }
 
         $view->setData([

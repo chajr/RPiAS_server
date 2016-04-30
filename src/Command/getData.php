9ba25ea87e0ffc8127a5cc090a2c7e3c2d8fa4da
chajr <chajr@bluetree.pl>
2016-04-30 14:43:10 +0200

    Added  some base logic to command get

--- src/Command/getData.php
+++ src/Command/getData.php
@@ -21,6 +21,7 @@ class getData
 
         $secureToken = (new Config)->getConfig()['secure_token'];
         $retrievedSecureToken = $request->query->get('key', '');
+        $host = $request->query->get('host', '');
 
         if ($secureToken !== $retrievedSecureToken) {
             $status = 'error';
@@ -37,5 +38,22 @@ class getData
         ]);
 
         $response->content->set($view->__invoke());
+    }
+
+    protected function _getCommands($host)
+    {
+        $query = (new\Database\Query)
+            ->select()
+            ->from('commands')
+            ->cols([
+                'command_id',
+                'command',
+                'to_be_exec',
+            ])
+            ->where('executed = 0')
+            ->where('consumed = 0')
+            ->where("host = '$host'");
+
+        return (new Connect)->query($query);
     }
 }

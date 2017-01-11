10b57c13c7554371ef28e62ffdad0c48c9ed3d06
chajr <chajr@bluetree.pl>
2017-01-11 16:09:27 +0100

    Save execution status in database

--- src/Command/setData.php
+++ src/Command/setData.php
@@ -27,24 +27,8 @@ class setData
             $status = 'error';
             $message = 'Incorrect secure token.';
         } else {
-            $post = $request->post;
-            $commandId = $post->get('command_id', null);
-            $consumedDate = $post->get('command_consumed_date_time', '0000-00-00 00:00:00');
-            $mongoId = $post->get('mongo_id', null);
-
-            if ($commandId) {
-                $query = (new Query)
-                    ->update()
-                    ->table('commands')
-                    ->cols([
-                        'consumed' => 1,
-                        'command_consumed_date_time' => $consumedDate,
-                        'mongo_id' => $mongoId,
-                    ])
-                    ->where('command_id = ?', $commandId);
-
-                (new Connect)->query($query);
-            }
+            $this->markAsConsumed($request->post);
+            $this->setOutput($request->post);
         }
 
         $view->setData([
@@ -53,5 +37,65 @@ class setData
         ]);
 
         $response->content->set($view->__invoke());
+    }
+
+    /**
+     * @param \Aura\Web\Request\Values $post
+     */
+    protected function markAsConsumed($post)
+    {
+        if ($post->get('data_update', false)) {
+            return;
+        }
+
+        $commandId = $post->get('command_id', null);
+        $consumedDate = $post->get('command_consumed_date_time', '0000-00-00 00:00:00');
+        $mongoId = $post->get('mongo_id', null);
+
+        if ($commandId) {
+            $query = (new Query)
+                ->update()
+                ->table('commands')
+                ->cols([
+                    'consumed' => 1,
+                    'command_consumed_date_time' => $consumedDate,
+                    'mongo_id' => $mongoId,
+                ])
+                ->where('command_id = ?', $commandId);
+
+            (new Connect)->query($query);
+        }
+    }
+
+    /**
+     * @param \Aura\Web\Request\Values $post
+     */
+    protected function setOutput($post)
+    {
+        if ($post->get('data_update', false)) {
+            $currentTime = date('Y-m-d H:i:s');
+            $commandId = $post->get('command_id', null);
+
+            $executed = $post->get('executed', 0);
+            $output = $post->get('output', '');
+            $error = $post->get('error', 0);
+            $executionTime = $post->get('exec_time', $currentTime);
+
+            if ($commandId) {
+                $query = (new Query)
+                    ->update()
+                    ->table('commands')
+                    ->cols([
+                        'executed' => $executed,
+                        'response' => $output,
+                        'error' => $error,
+                        'exec_time' => $executionTime,
+                        'response_date_time' => $currentTime,
+                    ])
+                    ->where('command_id = ?', $commandId);
+
+                (new Connect)->query($query);
+            }
+        }
     }
 }

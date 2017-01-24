cc824ec06da622a4fbefc3dded96b8396d000bd7
chajr <chajr@bluetree.pl>
2017-01-24 18:09:13 +0100

    Code convention changes

--- web/index.php
+++ web/index.php
@@ -10,9 +10,13 @@
  */
 
 $path = dirname(__DIR__);
+
 require "{$path}/vendor/autoload.php";
+
+/** @var Aura\Web_Kernel\WebKernel $kernel */
 $kernel = (new \Aura\Project_Kernel\Factory)->newKernel(
     $path,
     'Aura\Web_Kernel\WebKernel'
 );
+
 $kernel();

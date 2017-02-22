c64596a23f972ba761fa6dee5b319d9ed4dfb477
chajr <chajr@bluetree.pl>
2017-02-22 17:30:15 +0100

    Added basic handler for main page

---
+++ src/Manager/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Manager;
+
+class Main
+{
+    /**
+     * @param \Aura\Web\Request $request
+     * @param \Aura\Web\Response $response
+     * @param \Aura\View\View $view
+     */
+    public function __construct($request, $response, $view)
+    {
+        $message = 'Main Page';
+
+        $view->setData([
+            'message' => $message,
+        ]);
+
+        $response->content->set($view->__invoke());
+    }
+}
---
+++ templates/views/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>RPiAS</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="/" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/base.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+
+    <h1><?=$this->message?></h1>
+
+<script src="js/vendor/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
+
+

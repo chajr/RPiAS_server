7ae9646d709c7c2a15bffcf18e77b9e26f0b3976
chajr <chajr@bluetree.pl>
2017-09-04 18:16:15 +0200

    Added basic files that allow to manage lights by web

---
+++ src/Light/Disable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Alert;
+
+use Log\Log;
+use Config\Config;
+use Command\Manage;
+
+class Disable
+{
+    /**
+     * setData constructor.
+     *
+     * @param \Aura\Web\Request $request
+     * @param \Aura\Web\Response $response
+     * @param \Aura\View\View $view
+     */
+    public function __construct($request, $response, $view)
+    {
+        $status = 'success';
+        $message = 'Light turned on.';
+        $manager = new Manage;
+
+        $secureToken = (new Config)->getConfig()['secure_token'];
+        $retrievedSecureToken = $request->query->get('key', '');
+
+        if ($secureToken !== $retrievedSecureToken) {
+            $status  = 'error';
+            $message = 'Incorrect secure token';
+        } else {
+            $request->post->host = 'rpi-mc';
+
+            $request->post->command = 'redis-cli set rpia_illuminate_force false';
+            $manager->setCommand($request->post);
+
+            $request->post->command = 'redis-cli set rpia_illuminate_status false';
+            $manager->setCommand($request->post);
+        }
+
+        $view->setData([
+            'status' => $status,
+            'message' => $message,
+        ]);
+
+        $response->content->set($view->__invoke());
+    }
+}
---
+++ src/Light/View.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Light;
+
+class View
+{
+
+}
